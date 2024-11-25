e2dac7301207906eee25538baa69ede56403a5b9
anastasiaciobanu <ciobanu.p.mariaanastasia23@stud.rau.ro>
2024-11-25 05:10:30 +0200

    WIP

---
+++ zodii/src/FunFactNL.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FunFactNL()
+{
+    return(
+        <><h1>Stiati ca...</h1>
+        <p>Planetele in retrograd (ne uitam la tine, Mercur)
+            nu iti aduc, de fapt, numai catastrofe? Ele ne indeamna
+            sa ne uitam in trecutul nostru si sa invatam lectiile specifice
+            acelei planete. Asa ca nu mai ai o scuza sa dai vina pe Mercur
+            in retrograd daca iti merg toate prost, mai degraba da vina pe tine si fa ceva in legatura cu asta</p></>
+    );
+}
+
+export default FunFactNL;
--- zodii/src/ZodiacTable.js
+++ zodii/src/ZodiacTable.js
@@ -3,7 +3,28 @@ import React from "react";
 function ZodiacTable()
 {
     return(
-        <p>Tabel</p>
+        <><p>Tabel</p><table>
+            <tr>
+                <td>Berbec</td>
+                <td>Taur</td>
+                <td>Gemeni</td>
+            </tr>
+            <tr>
+                <td>Rac</td>
+                <td>Leu</td>
+                <td>Fecioara</td>
+            </tr>
+            <tr>
+                <td>Balanta</td>
+                <td>Scorpion</td>
+                <td>Sagetator</td>
+            </tr>
+            <tr>
+                <td>Capricorn</td>
+                <td>Varsator</td>
+                <td>Pesti</td>
+            </tr>
+        </table></>
     );
 }
 
